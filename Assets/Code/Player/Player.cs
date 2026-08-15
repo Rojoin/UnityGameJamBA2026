183cd184be941602d094ce9a65a51c54eb0b658a
Rojoin <nachoarrastua@gmail.com>
2026-08-15 12:35:40 -0300

    Add player actions

--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -7,32 +7,68 @@ namespace Code.Player
 	[RequireComponent(typeof(CharacterController))]
 	public class Player : MonoBehaviour
 	{
-		[SerializeField] private InputAction movementInputAction;
-		[SerializeField] private InputAction cameraMovementInputAction;
-		[SerializeField] private InputAction clickInputAction;
-		[SerializeField] private InputAction secondClickInputAction;
+		[SerializeField] private InputActionReference movementInputAction;
+		[SerializeField] private InputActionReference cameraMovementInputAction;
+		[SerializeField] private InputActionReference clickInputAction;
+		[SerializeField] private InputActionReference secondClickInputAction;
 		[SerializeField] private float movementSpeed = 10f;
 		private Vector2 movementInput = new Vector2(0,0);
 		private CharacterController controller;
 
-
+		private Camera camera;
 		private void Awake()
 		{
 			controller = GetComponent<CharacterController>();
+			movementInputAction.action.Enable();
+			cameraMovementInputAction.action.Enable();
+			clickInputAction.action.Enable();
+			secondClickInputAction.action.Enable();
+
+			clickInputAction.action.performed += OnPrimaryClick;
+			secondClickInputAction.action.performed += OnSecondaryClick;
+			camera = Camera.main;
+		}
+
+		private void OnSecondaryClick(InputAction.CallbackContext obj)
+		{
+			
+		}
+
+		private void OnPrimaryClick(InputAction.CallbackContext obj)
+		{
+			
 		}
 
 		private void Update()
 		{
 			movementInput = Vector2.zero;
-			if (movementInputAction.ReadValue<Vector2>() != Vector2.zero)
+			if (movementInputAction.action.ReadValue<Vector2>() != Vector2.zero)
 			{
-				movementInput = movementInputAction.ReadValue<Vector2>();
+				movementInput = movementInputAction.action.ReadValue<Vector2>();
 			}
 
 			if (movementInput != Vector2.zero)
 			{
 				controller.Move( new Vector3(movementInput.x, 0, movementInput.y) * (movementSpeed * Time.deltaTime) );
 			}
+
+			if (cameraMovementInputAction.action.ReadValue<Vector2>() != Vector2.zero)
+			{
+
+				Vector2 inputDir;
+				camera.transform.rotation = Quaternion.Euler(cameraMovementInputAction.action.ReadValue<Vector2>().x, 0, 0);
+			}
+		}
+
+		private void OnDestroy()
+		{
+			movementInputAction.action.Disable();
+			cameraMovementInputAction.action.Disable();
+			clickInputAction.action.Disable();
+			secondClickInputAction.action.Disable();
+			
+			clickInputAction.action.performed -= OnPrimaryClick;
+			secondClickInputAction.action.performed -= OnSecondaryClick;
 		}
 	}
 }
