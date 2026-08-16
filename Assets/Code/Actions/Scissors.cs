77f62961b10c5c7d15f3efa2c06ab5578c1cca08
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2026-08-16 17:47:46 -0300

    Add damageable interface

--- Assets/Code/Actions/Scissors.cs
+++ Assets/Code/Actions/Scissors.cs
@@ -1,34 +1,88 @@
-namespace Code.Actions
+using UnityEngine;
+
+public sealed class Scissors : Item
 {
-	public class Scissors : Item
+	[SerializeField] private Transform hand;
+	[SerializeField] private GameObject scissorsVisual;
+	[SerializeField] private SphereCollider attackCollider;
+	[SerializeField] private float attackDuration = 0.1f;
+	[SerializeField] private float attackCooldown = 0.5f;
+	[SerializeField] private float damage = 1f;
+
+	private float nextAttackTime;
+	private bool isAttacking;
+
+	public override ItemType ItemType => ItemType.Scissors;
+
+	private void Awake()
 	{
-		public override ItemType ItemType => ItemType.Scissors;
+		attackCollider.enabled = false;
+		scissorsVisual.SetActive(false);
+	}
 
-		public override void Activate()
+	public override void Activate()
+	{
+		scissorsVisual.SetActive(true);
+
+		transform.SetParent(hand);
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+	}
+
+	public override void Deactivate()
+	{
+		scissorsVisual.SetActive(false);
+		attackCollider.enabled = false;
+		isAttacking = false;
+	}
+
+	public override void PrimaryAction()
+	{
+		if (isAttacking || Time.time < nextAttackTime)
 		{
-
+			return;
 		}
-		public override void Deactivate()
+
+		isAttacking = true;
+		nextAttackTime = Time.time + attackCooldown;
+
+		attackCollider.enabled = true;
+
+		Invoke(nameof(EndAttack), attackDuration);
+	}
+
+	public override void PrimaryActionReleased()
+	{
+	}
+
+	public override void SecondaryAction()
+	{
+	}
+
+	public override void SecondaryActionReleased()
+	{
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!isAttacking)
 		{
-
+			return;
 		}
-		
-		public override void PrimaryAction()
+
+		IDamageable damageable = other.GetComponent<IDamageable>();
+
+		if (damageable == null)
 		{
-
+			return;
 		}
 
-        public override void PrimaryActionReleased()
-        {
-        }
+		damageable.TakeDamage(damage);
+	}
 
-        public override void SecondaryAction()
-		{
-
-		}
-
-        public override void SecondaryActionReleased()
-        {
-        }
-    }
+	private void EndAttack()
+	{
+		attackCollider.enabled = false;
+		isAttacking = false;
+	}
 }
